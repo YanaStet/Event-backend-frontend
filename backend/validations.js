d32a8ab7dc45e5f2d2d499b3595a4c12d54a9b88
Yana <yanastetforcomp@gmail.com>
2025-05-25 21:37:28 +0300

    Завершила версту і сайт впринципі готовий

--- backend/validations.js
+++ backend/validations.js
@@ -2,17 +2,16 @@ import {body} from 'express-validator';
 
 export const registerValidator = [
     body('email', 'Некоректний email').isEmail(),
-    body('password', 'Некоректний пароль').isLength({min: 5}),
+    body('password', 'Некоректний пароль').isLength({min: 8}),
     body('fullName', "Некоректне ім'я").isLength({min: 3}),
 ];
 
 export const loginValidator = [
     body('email', 'Некоректний email').isEmail(),
-    body('password', 'Некоректний пароль').isLength({min: 5})
+    body('password', 'Некоректний пароль').isLength({min: 8})
 ];
 
 export const postCreateValidator = [
     body('email', 'Некоректний email').isEmail(),
     body('fullName', "Некоректне ім'я").isLength({min: 3}),
-    body('descr', "Некоректний опис").isLength({min: 10}).isString(),
 ];
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,27 +1,28 @@
 document.addEventListener('DOMContentLoaded', async () => {
 
-    const regModal = document.querySelector("#register");
-    const logModal = document.querySelector("#login");
+    const registerModal = document.querySelector('#register');
+    const loginModal = document.querySelector("#login");
     const logBtns = document.querySelectorAll('.log');
     const regBtns = document.querySelectorAll('.reg');
 
     function showRegModal() {
-        regModal.classList.add('active');
+        registerModal.classList.add('active');
         document.body.style.overflowY = 'hidden';
     }
 
     function closeRegModal() {
-        regModal.classList.remove('active');
+        registerModal.classList.remove('active');
         document.body.style.overflowY = '';
     }
 
     function showLogModal() {
-        logModal.classList.add('active');
+        loginModal.classList.add('active');
         document.body.style.overflowY = 'hidden';
     }
 
     function closeLogModal() {
-        logModal.classList.remove('active');
+        console.log('close');
+        loginModal.classList.remove('active');
         document.body.style.overflowY = '';
     }
 
@@ -37,25 +38,36 @@ document.addEventListener('DOMContentLoaded', async () => {
             showRegModal()
         });
     });
-    regModal.addEventListener('click', (e) => {
-        const wrap = document.querySelector('.modal__wrapper');
-        if (e.target != wrap) {
-            closeRegModal();
-        }
-    });
-    logModal.addEventListener('click', (e) => {
-        const wrap = document.querySelector('.modal__wrapper');
-        if (e.target != wrap) {
-            closeLogModal();
-        }
-    });
 
 
+    // бд
+
+    const messages = {
+        success: "Thank you, for participating!",
+        failure: "Something went wrong..." 
+    }
+    let message;
+
+    const showResponse = (message, modalWrapper, modalContent) => {
+        return new Promise(resolve => {
+            const wrong = document.createElement('div');
+            wrong.innerHTML = `${message}`;
+            wrong.classList.add('.modal__title');
+
+            modalContent.style.display = 'none';
+            modalWrapper.appendChild(wrong);
+            setTimeout(() => {
+                modalContent.style.display = 'flex';
+                modalWrapper.removeChild(wrong);
+            }, 3000);
+        });
+    };
 
     async function resetCards(wrapperSelector) {
         const wrapper = document.querySelector(wrapperSelector);
         const response = await fetch('http://localhost:3000/posts');
         const data = await response.json();
+        wrapper.innerHTML = '';
         if (data) {
             data.forEach(item => {
                 wrapper.innerHTML += `
@@ -70,6 +82,109 @@ document.addEventListener('DOMContentLoaded', async () => {
             });
         }
     }
+    const cardPost = async (obj) => {
+        const response = await fetch('http://localhost:3000/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(obj),
+        });
+        const res = await response.json();
+        resetCards('.joined__people');
+    };
+    const userRegister = async (obj) => {
+        const response =  await fetch('http://localhost:3000/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(obj),
+        });
+        const data = await response.json();
+        return data;
+    };
+    const userLogin = async (obj) => {
+        const response = await fetch('http://localhost:3000/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(obj),
+        });
+        const data = await response.json();
+        return data;
+    };
 
+
+
+
+    registerModal.addEventListener('click', async (e) => {
+        const wrap = document.querySelector('.modal .reg');
+        if (!wrap.contains(e.target || !wrap)) {
+            closeRegModal();
+        } else {
+            if (e.target == document.querySelector('.modal #regSubmit')) {
+                e.preventDefault();
+
+                const formData = new FormData(registerModal);
+                const data = Object.fromEntries(formData.entries());
+
+                const obj = {
+                    fullName: data.nameReg,
+                    email: data.emailReg,
+                    password: data.passwordReg
+                };
+
+                const res = await userRegister(obj);
+                registerModal.reset();
+                if (res.fullName) {
+                    const req = {
+                        user: res.fullName,
+                        email: res.email
+                    }
+                    cardPost(req);
+                    const welcome = document.querySelector('.header__welcome-text');
+                    welcome.innerHTML = `Hello, ${res.fullName}!`;
+                    message = messages.success;
+                    await showResponse(message, document.querySelector('.modal .reg'), document.querySelector('.reg__content'));
+                } else {
+                    message = messages.failure;
+                    await showResponse(message, document.querySelector('.modal .reg'), document.querySelector('.reg__content'));
+                }
+            }
+        }
+    });
+    loginModal.addEventListener('click', async (e) => {
+        const wrap = document.querySelector('.modal .log');
+        if (!wrap.contains(e.target)) {
+            closeLogModal();
+        } else {
+            if (e.target == document.querySelector('.modal #logSubmit')) {
+                e.preventDefault();
+
+                const formData = new FormData(loginModal);
+                const data = Object.fromEntries(formData.entries());
+
+                const obj = {
+                    email: data.emailLog,
+                    password: data.passwordLog,
+                };
+
+                const res = await userLogin(obj);
+                loginModal.reset();
+                if (res.fullName) {
+                    const welcome = document.querySelector('.header__welcome-text');
+                    welcome.innerHTML = `Hello, ${res.fullName}!`;
+                    message = messages.success;
+                    await showResponse(message, document.querySelector('.modal .log'), document.querySelector('.log__content'));
+                } else {
+                    message = messages.failure;
+                    await showResponse(message, document.querySelector('.modal .log'), document.querySelector('.log__content'));
+                }
+            }
+        }
+    });
+    
     resetCards('.joined__people');
 });
